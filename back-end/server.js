eaa7a8288e6a373e27df2fe524dde632451847a3
pricillia <pricillia.edou@gmail.com>
2024-10-25 11:27:44 +0200

    création de l'endpoint /produits dans le back-end

--- back-end/server.js
+++ back-end/server.js
@@ -25,10 +25,28 @@ db.connect((err) => {
 });
 // Route en GET
 
-// Utilisation d'un try permettant d'exécuter du code susceptible de déclencher une erreur. Si une erreur se produit, elle sera interceptée par un bloc catch.
+// Utilisation d'un try permettant d'exécuter du code susceptible de déclencher une erreur. Si une erreur se produit, elle est interceptée par un bloc catch.
 
 app.get("/produits", (req, res) => {
-  res.status(200).send("Hello, je teste le serveur 🔥");
+  const sql = "SELECT * FROM produits";
+
+  try {
+    db.query(sql, (err, result) => {
+      if (!err) {
+        console.log("Opération réussie 🔥");
+        return res.json(result); // Requête réussie, réponse envoyée en JSON
+      }
+
+      // Si une erreur survient, elle est levée pour être capturée par le bloc catch
+      //on peut faire throw err; // seulement on ne pourra pas personnalisé le msg de l'erreur
+      throw new Error(
+        "Erreur lors de l'exécution de la requête 😥: " + err.message
+      );
+    });
+  } catch (error) {
+    console.error("Erreur lors de la récupération des produits:", error); // Utilisation de `error` et non `err`
+    res.status(500).send("Erreur lors de la récupération des produits");
+  }
 });
 
 app.all("*", function (req, res) {
